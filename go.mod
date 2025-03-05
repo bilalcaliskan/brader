9a9208f4e00389b64e243c045a2ef1afee66735d
bilalcaliskan <bilalcaliskan@protonmail.com>
2025-03-06 01:19:56 +0300

    chore: hello this is me

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/bilalcaliskan/brader
+
+go 1.24.0
--- main.go
+++ main.go
@@ -5,10 +5,5 @@ import (
 )
 
 func main() {
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
-
-	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
-	}
+	fmt.Println("welcome to brader")
 }
